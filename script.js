52dfc0ab37d0698d2557cf1879165ea8aac6dfe5
Peter <peters1989@mail.ru>
2022-07-16 22:27:27 +0500

    Js-возведение в степень 2

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
         num = num * a
     }
     alert(num);
-  }else if (!isNaN(a) && !isNaN (b) && b == 0 ){
+  }else if (!isNaN(a) && !isNaN (b) && a != 0 && b == 0 ){
       
     
     for(let i = 1; i <= (b = 2); i++ ) {
@@ -17,10 +17,31 @@
     }
     alert(num);
   } else if (!isNaN(a) && !isNaN(b) && a == 0 && b != 0){
-      alert('Первое число не далжно быть ( 0 ) введите коректное число')
+
+    let a = +prompt('Введите число не равное ( 0 )');
+    let b = +prompt('Введите степень в которую нужно возвести число');
+
+
+    for(let i = 1; i <= (b = 2); i++ ) {
+        num = num * a
+    }
+    alert(num);
+      
+  } else if (!isNaN(a) && !isNaN(b) && a == 0 && b == 0){
+    let a = +prompt('Введите число не равное ( 0 )');
+    let b = +prompt('Введите степень в которую нужно возвести число');
+
+
+    for(let i = 1; i <= (b = 2); i++ ) {
+        num = num * a
+    }
+    alert(num);
   }
 
 
+
+
+  
 
 
   
